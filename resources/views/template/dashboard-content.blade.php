adda00c4a3cb67de5e6eafbeb74f51fb2520b9b8
waseem ali <60802736+aliw2025@users.noreply.github.com>
2022-08-25 20:30:39 +0500

    first commit

--- resources/views/template/dashboard-content.blade.php
+++ resources/views/template/dashboard-content.blade.php
@@ -6,11 +6,151 @@
     <div class="content-body">
         <section id="dashboard-ecommerce">
 
-            
-          
+            <div class=" card-statistics">
+                <div class="card-header">
+                    <h4 class="card-title">Statistics</h4>
+                    <div class="d-flex align-items-center">
+                        <p class="card-text font-small-2 me-25 mb-0">Updated 1 sec ago</p>
+                    </div>
+                </div>
+                <div class="card-body statistics-body">
+                    <div class="row">
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0 card card p-2 me-2 p-2 me-2 ">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Sales</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0 card card p-2 me-2 p-2">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-danger me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Sales</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0   card p-2 me-2 ">
+                          <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Recievables</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0  card p-2 me-2 ">
+                           <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Payables</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0  card p-2 me-2">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Expenses</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0 mt-1 card p-2 me-2">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Assets</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0 mt-1 card p-2 me-2">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Unrealized Profit</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-sm-6 col-12 mb-2 mb-xl-0 mt-1 card p-2 me-2">
+                            <div class="d-flex flex-row">
+                                <div class="avatar bg-light-primary me-2">
+                                    <div class="avatar-content">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon">
+                                            <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+                                            <polyline points="17 6 23 6 23 12"></polyline>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="my-auto">
+                                    <h4 class="fw-bolder mb-0">230k</h4>
+                                    <p class="card-text font-small-3 mb-0">Charity</p>
+                                </div>
+                            </div>
+                        </div>
+                       
+                       
+                    </div>
+                </div>
+            </div>
+
         </section>
-    
+
     </div>
 </div>
 @endsection
-
