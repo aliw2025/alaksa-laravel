22aa3b7f686e8c65eda769fd7002ca25be32cfcb
waseem ali <aliw2025@gmail.com>
2023-04-26 16:58:42 +0500

    roles permission not completed

--- resources/views/admin/roles-permissions.blade.php
+++ resources/views/admin/roles-permissions.blade.php
@@ -46,8 +46,8 @@
                                             {{$role->name}}
                                         </td>
                                         <td>
-                                        <!-- onclick="roleSelected()" -->
-                                            <a data-role='{{$role->name}}'  class="role"  href="#">select</a>
+                                            <!-- onclick="roleSelected()" -->
+                                            <a data-role='{{$role->name}}' class="role" href="#">select</a>
                                         </td>
 
                                     </tr>
@@ -69,62 +69,66 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            
+
                             <div class="row">
-                                <div class="col-6">
-                                 <input type="hidden" name="role_id" class="form-control">
+                            <form method="POST" class="form form-vertical" autocomplete="on" action="{{route('role-permissions')}}">
+                                    <div class="col-6">
+                                        <input type="hidden" name="role_id" class="form-control">
 
-                                    <input type="text" class="form-control">
-                                </div>
-                                <div class="col-6">
-                                    <select class="form-control" name="permission" id="">
-                                        @foreach($permissions as $permission)
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <select class="form-control" name="permission" id="">
+                                            @foreach($permissions as $permission)
                                             <option value="{{$permission->id}}">{{$permission->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-12">
 
-                                <button class="mt-2  btn btn-primary">ADd</button>
+                                        <button class="mt-2  btn btn-primary">ADd</button>
+                                    </div>
+                                </form>
+
                             </div>
+
 
                             <div class="mt-2">
-                            <table id="investor-table" class="table">
-                                <thead class="thead-dark">
-                                    <tr style="background-color:red !important;">
-                                        <th style="width: 2px !important">#</th>
-                                        <th scope="col">Role Name</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="inventory-iems-body" id="nventory-iems-body">
-                                    @If(isset($roles))
-                                    @php
-                                    $count = 1;
-                                    @endphp
-                                    @foreach($roles as $role)
-                                    <tr>
-                                        <td>
-                                            {{$count}}
-                                        </td>
-                                        <td>
-                                            <!-- <a href="">{{$role->name}}</a> -->
-                                            {{$role->name}}
-                                        </td>
-                                        <td>
-                                        <!-- onclick="roleSelected()" -->
-                                            <a data-role='{{$role->name}}'  class="role"  href="#">select</a>
-                                        </td>
+                                <table id="investor-table" class="table">
+                                    <thead class="thead-dark">
+                                        <tr style="background-color:red !important;">
+                                            <th style="width: 2px !important">#</th>
+                                            <th scope="col">Role Name</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="inventory-iems-body" id="nventory-iems-body">
+                                        @If(isset($roles))
+                                        @php
+                                        $count = 1;
+                                        @endphp
+                                        @foreach($roles as $role)
+                                        <tr>
+                                            <td>
+                                                {{$count}}
+                                            </td>
+                                            <td>
+                                                <!-- <a href="">{{$role->name}}</a> -->
+                                                {{$role->name}}
+                                            </td>
+                                            <td>
+                                                <!-- onclick="roleSelected()" -->
+                                                <a data-role='{{$role->name}}' class="role" href="#">select</a>
+                                            </td>
 
-                                    </tr>
-                                    @php
-                                    $count+= 1;
-                                    @endphp
-                                    @endforeach
-                                    @endif
-                                </tbody>
-                            </table>
+                                        </tr>
+                                        @php
+                                        $count+= 1;
+                                        @endphp
+                                        @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -134,22 +138,16 @@
     </div>
 </div>
 <script>
-
-    $('.role').on('click',function(){
-
-
+    $('.role').on('click', function() {
         // console.log('waseem');
         console.log(this);
-
-
-
     });
 
-    function roleSelected(){
+
+    function roleSelected() {
 
         console.log('role selected');
 
     }
-
 </script>
 @endsection
