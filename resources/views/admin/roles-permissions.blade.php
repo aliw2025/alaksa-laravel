2d05f070457f5b63b0bfac64935e781e64133653
waseem ali <aliw2025@gmail.com>
2023-04-26 16:54:26 +0500

    working on permissions

--- resources/views/admin/roles-permissions.blade.php
+++ resources/views/admin/roles-permissions.blade.php
@@ -65,12 +65,67 @@
                     <div class="card">
                         <div class="card-header">
                             <div>
-                                <h4 class="text-center">Permission</h4>
+                                <h4 class="text-center">Add Permission</h4>
                             </div>
                         </div>
                         <div class="card-body">
                             
+                            <div class="row">
+                                <div class="col-6">
+                                 <input type="hidden" name="role_id" class="form-control">
 
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-6">
+                                    <select class="form-control" name="permission" id="">
+                                        @foreach($permissions as $permission)
+                                            <option value="{{$permission->id}}">{{$permission->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div>
+
+                                <button class="mt-2  btn btn-primary">ADd</button>
+                            </div>
+
+                            <div class="mt-2">
+                            <table id="investor-table" class="table">
+                                <thead class="thead-dark">
+                                    <tr style="background-color:red !important;">
+                                        <th style="width: 2px !important">#</th>
+                                        <th scope="col">Role Name</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="inventory-iems-body" id="nventory-iems-body">
+                                    @If(isset($roles))
+                                    @php
+                                    $count = 1;
+                                    @endphp
+                                    @foreach($roles as $role)
+                                    <tr>
+                                        <td>
+                                            {{$count}}
+                                        </td>
+                                        <td>
+                                            <!-- <a href="">{{$role->name}}</a> -->
+                                            {{$role->name}}
+                                        </td>
+                                        <td>
+                                        <!-- onclick="roleSelected()" -->
+                                            <a data-role='{{$role->name}}'  class="role"  href="#">select</a>
+                                        </td>
+
+                                    </tr>
+                                    @php
+                                    $count+= 1;
+                                    @endphp
+                                    @endforeach
+                                    @endif
+                                </tbody>
+                            </table>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -85,7 +140,7 @@
 
         // console.log('waseem');
         console.log(this);
-        
+
 
 
     });
