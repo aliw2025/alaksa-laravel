ee92b27bfde370f4122cbc838da7a0d564f489ec
waseem ali <aliw2025@gmail.com>
2023-05-16 12:14:42 +0500

    just syncing

--- app/Models/Payable.php
+++ app/Models/Payable.php
@@ -9,35 +9,10 @@ class Payable extends Model
 {  
     use HasFactory;
 
-//     public function purchase()
-//     {
-//             return $this->belongsTo(Purchase::class,'transaction_id');
-//     }
-
-    public function investor()
+    public function purchase()
     {
-            return $this->belongsTo(Investor::class,'investor_id');
+            return $this->belongsTo(Purchase::class,'transaction_id');
     }
 
-
-    public function supplier_val()
-    {       
-        
-            return $this->belongsTo(Supplier::class,'supplier');
-    }
     
-    public function leadgerEntries(){
-
-        return $this->morphMany(GLeadger::class,'transaction');
-    }
-    public function createLeadgerEntry($accound_id,$value,$investor_id,$date,$user_id ){
-
-        $this->leadgerEntries()->create([
-            'account_id' => $accound_id,
-            'value' => $value,
-            'investor_id' => $investor_id,
-            'date' => $date,
-            'user_id'=>$user_id
-        ]);
-    }
 }
